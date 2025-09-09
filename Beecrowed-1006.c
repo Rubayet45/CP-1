8cbd3c4e83da532affcdad17ef67d186298bb079
Rubayet45 <rubayetjoynul@gmail.com>
2025-09-09 06:39:08 +0600

    Add files via upload

---
+++ Beecrowed-1006.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+double A,B,C;
+scanf("%lf",&A);
+scanf("%lf",&B);
+scanf("%lf",&C);
+int sum = 2+3+5;
+double avg=( A * 2 + B * 3 + C * 5) / sum;
+printf("MEDIA = %.1lf\n",avg);
+
+return 0;
+}
+
