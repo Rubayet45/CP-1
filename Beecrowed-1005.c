7b7c8456fedefc1982c4df7010b45be9e85e95c4
Rubayet45 <rubayetjoynul@gmail.com>
2025-09-09 06:38:27 +0600

    Add files via upload

---
+++ Beecrowed-1005.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+float A,B;
+scanf("%f",&A);
+scanf("%f",&B);
+float sum = 3.5+7.5;
+float avg=( A * 3.5 + B * 7.5) / sum;
+printf("MEDIA = %.5f\n",avg);
+
+return 0;
+}
+
