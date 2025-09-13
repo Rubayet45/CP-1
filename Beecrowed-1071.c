5ddc2eaa7e51c6cc9f19a3748ea38c318863c541
Rubayet45 <rubayetjoynul@gmail.com>
2025-09-13 20:08:03 +0600

    Add files via upload

---
+++ Beecrowed-1071.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main (){
+int a,b;
+int sum=0;
+scanf("%d%d",&a,&b);
+{
+for(int i=b+1;i<a;i++){
+    if(i%2!=0){
+        sum=sum+i;}
+}}
+printf("%d\n",sum);
+return 0;
+}
+
